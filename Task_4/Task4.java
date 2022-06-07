27d4aafab6224daa9fc34769acddc6b57c73dd4b
Маис <Kozannostra777@mail.ru>
2022-06-07 15:39:34 +0300

    Некоторый код Ивана

--- Task_4/Task4.java
+++ Task_4/Task4.java
@@ -19,4 +19,21 @@ public class Task4 {
 
 
 
+    // Код Ивана П.
+    // public static void move (char pos1, char pos2) {// Сначала рассмотрим, как двигаться, когда есть только одна пластина
+    //     System.out.print (pos1 + "==>" + pos2 + ""); // непосредственно перемещаем пластину в целевую область
+    //     }
+        
+    //     public static void hanio(int n,char pos1,char pos2,char pos3) {
+    //     if(n == 1) {
+    //     move(pos1,pos3);
+    //     } else {
+    //     hanio(n-1,pos1,pos3,pos2);
+    //     move(pos1,pos3);
+    //     hanio(n-1,pos2,pos1,pos3);
+    //     }
+    //     }
+
+
+
     
