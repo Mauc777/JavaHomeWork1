b92e57e5b41ad5c6c2aafe3d182f89ca31b33e73
Маис <Kozannostra777@mail.ru>
2022-06-07 15:20:13 +0300

    Задача 4(Ханойские Башни) НЕДОРЕШЕННАЯ

---
+++ Task_4/Task4.java
@@ -0,0 +1,23 @@
+package Task_4;
+public class Task4 {
+    public static void main(String[] args) {
+        Sort(1,2,3,3);
+    }
+
+
+    static void Sort(int from, int to, int other, int count){
+        System.out.printf("%d -> %d\n", from,other);
+        if(count>0){
+        if(from>other)Sort(other,to,from,count-1);
+        if(from>to)Sort(to,from,other,count-1);
+        if(other>to)Sort(from,other,to,count-1);
+
+        }
+
+    }
+        
+    }
+
+
+
+    
