7ab4decfb95ffdc8f722f1b73bec28080f45eb32
Маис <Kozannostra777@mail.ru>
2022-06-20 16:02:01 +0300

    Черновик и пробный вариант решения ханой

--- Task_4/Task4.java
+++ Task_4/Task4.java
@@ -1,35 +1,61 @@
+// package Task_4;
 
-// в зависимости от того, где он меньше. Тогда, чтобы решить головоломку с чётным количеством дисков,
-//  надо многократно повторять действия: 1-2, 1-3, 2-3.
-//  Если число дисков нечётно — 1-3, 1-2, 2-3.
-package Task_4;
-public class Task4 {
-    public static void main(String[] args) {
-        Sort(1,2,3,3);
-    }
-    static void Sort(int from, int to, int other, int count){
-        System.out.printf("%d -> %d\n", from,other);
-        if(count>0){
-        if(from>other)Sort(other,to,from,count-1);
-        if(from>to)Sort(to,from,other,count-1);
-        if(other>to)Sort(from,other,to,count-1);
-        }
-    }  
-    }
+// // в зависимости от того, где он меньше. Тогда, чтобы решить 
+// // головоломку с чётным количеством дисков,
+// //  надо многократно повторять действия: 1-2, 1-3, 2-3.
+// //  Если число дисков нечётно — 1-3, 1-2, 2-3.
 
 
+// // ToDOO Завершить после разбора 4ой лекции!! Она важнеее!!
+// public class Task4 {
 
-    // Код Ивана П.
-    // public static void move (char pos1, char pos2) {// Сначала рассмотрим, как двигаться, когда есть только одна пластина
-    //     System.out.print (pos1 + "==>" + pos2 + ""); // непосредственно перемещаем пластину в целевую область
-    //     }
+//     public static void main(String[] args) {
+//         Muve an = new Muve(3);  
+//         // an.setN(3);
+//         an.getN();
+//         System.out.println("Колец => " +an.getN());
+
+//         Muve bS = new Muve(1);
+//         bS.getN();
+//         System.out.println("Стартовый шпиль => " +bS.getN());
+
+//         Muve cF = new Muve(2);
+//         cF.getN();
+//         System.out.println("Финишный шпиль => " +cF.getN());
+
+//         // Muve.MuveS(an,bS,cF);
         
-    //     public static void hanio(int n,char pos1,char pos2,char pos3) {
-    //     if(n == 1) {
-    //     move(pos1,pos3);
-    //     } else {
-    //     hanio(n-1,pos1,pos3,pos2);
-    //     move(pos1,pos3);
-    //     hanio(n-1,pos2,pos1,pos3);
-    //     }
-    //     }
+//     }
+// }
+    
+//     // static double  MuveS(Muve an, Muve bS ,Muve cF) {
+//     //     if(an.getN()==1){
+//     //         return MuveS(an, bS, cF);
+//     //     } 
+//         // else {
+//         //     return TempOritMo(6, 3, 2);
+            
+//         // }
+
+        
+    
+//     // static double TempOritMo(int Temp1, int bS, int cF){
+//     //     int tmp = 0;
+//     //     tmp = (Temp1 - bS- cF);
+//     //     return tmp;
+//     // }
+//         // if (n == 1){
+//             // System.out.printf("%d ", n , Start1 ,  Finish1);
+//         // } 
+//         // else Muve(n-1, Start, Finish1); {
+//             // System.out.printf("%d " + "%d " + "%d ", n,  Start1, Finish1);
+//         //     int Temp1 =  6 - Start1 - Finish1;
+//         //     Muve(n-1, Temp1, Finish1);
+//         // }
+         
+    
+
+    
+
+  
+
