a5708897cc699ee766befefee22ce82d0888ea0b
Маис <Kozannostra777@mail.ru>
2022-06-24 20:48:09 +0300

    Первый коммит2

--- Task_4/Muve.java
+++ Task_4/Muve.java
@@ -1,70 +1,66 @@
-// package Task_4;
+package Task_4;
 
-// public class Muve {
-//     // private int n;
-//     // private String Start1;
-//     // private String Spair2;
-//     // private String Finish3;
-//      int n;
-//      String Start1;
-//      String Spair2;
-//      String Finish3;
+public class Muve {
+     int n;
+     String Spair1;
+     String Spair2;
+     String Spair3;
 
-//             //арг       count        spair1             spair2                spair3
-//     public Muve(int valueN, String valueStart, String valueTemp, String valueFinish) {
-//         this.n = valueN; 
-//         this.Start1 = valueStart;
-//         this.Spair2 = valueTemp;
-//         this.Finish3 = valueFinish;
-//     }
+            //арг      count        spair1             spair2                spair3
+    public Muve(int valueN, String valueStart, String valueTemp, String valueFinish) {
+        this.n = valueN; 
+        this.Spair1 = valueStart;
+        this.Spair1 = valueTemp;
+        this.Spair3 = valueFinish;
+    }
 
    
-//     // public Muve( var value) {
-//     //     this(3,1,2,3);
+    // public Muve( var value) {
+    //     this(3,1,2,3);
         
-//     // }
+    // }
    
 
-//     public Muve(var  value) {
-//         this(value);
-//     }
-//     public Muve() {
-//         this(3,2,1,3);
-//     }
+    public Muve(var  value) {
+        this(value);
+    }
+    public Muve() {
+        this(3,2,1,3);
+    }
 
 
-//     // public static void MuveS(Muve an, Muve bS, Muve cF) {
-//     // }
+    // public static void MuveS(Muve an, Muve bS, Muve cF) {
+    // }
 
-//     // public Muve(int value){
-//     //     this(value,value,value);
-//     // }
+    // public Muve(int value){
+    //     this(value,value,value);
+    // }
     
-//     public int getN(){
-//         return n;
-//     }
-//     public String getStart(){
-//         return Start1;
-//     }
-//     public String getSpairTemp(){
-//         return Spair2 ;
-//     }
-//     public String getFinish(){
-//         return Finish3;
-//     }
+    public int getN(){
+        return n;
+    }
+    public String getStart(){
+        return Spair1;
+    }
+    public String getSpairTemp(){
+        return Spair1 ;
+    }
+    public String getFinish(){
+        return Spair3;
+    }
 
-//     public void setN(int value){
-//         this.n = value;
-//     }
-//     public void setStart(String value){
-//         this.Start1 = value;
-//     }
-//     public void setSpairTemp(String value){
-//         this.Spair2 = value;
-//     }
-//     public void setFinish(String value){
-//         this.Finish3 = value;
-//     }
+    public void setN(int value){
+        this.n = value;
+    }
+    public void setStart(String value){
+        this.Spair1 = value;
+    }
+    public void setSpairTemp(String value){
+        this.Spair1 = value;
+    }
+    public void setFinish(String value){
+        this.Spair3 = value;
+    }
    
 
 
@@ -73,4 +69,4 @@
 
 
   
-// }    
+}    
--- Task_4/Task4.java
+++ Task_4/Task4.java
@@ -1,59 +1,63 @@
-// package Task_4;
+package Task_4;
 
-// // в зависимости от того, где он меньше. Тогда, чтобы решить 
-// // головоломку с чётным количеством дисков,
-// //  надо многократно повторять действия: 1-2, 1-3, 2-3.
-// //  Если число дисков нечётно — 1-3, 1-2, 2-3.
+// в зависимости от того, где он меньше. Тогда, чтобы решить 
+// головоломку с чётным количеством дисков,
+//  надо многократно повторять действия: 1-2, 1-3, 2-3.
+//  Если число дисков нечётно — 1-3, 1-2, 2-3.
 
 
-// // ToDOO Завершить после разбора 4ой лекции!! Она важнеее!!
-// public class Task4 {
+// ToDOO Завершить после разбора 4ой лекции!! Она важнеее!!
+public class Task4 {
 
-//     public static void main(String[] args) {
-//         Muve an = new Muve(3);  
-//         // an.setN(3);
-//         an.getN();
-//         System.out.println("Колец => " +an.getN());
+    public static void main(String[] args) {
+        Muve aN = new Muve(3);  
+        // an.setN(3);
+        aN.getN();
+        System.out.println("Колец => " +aN.getN());
 
-//         Muve bS = new Muve(1);
-//         bS.getN();
-//         System.out.println("Стартовый шпиль => " +bS.getN());
+        Muve aSp1 = new Muve(1);
+        aSp1.getN();
+        System.out.println("Стартовый шпиль => " +aSp1.getN());
 
-//         Muve cF = new Muve(2);
-//         cF.getN();
-//         System.out.println("Финишный шпиль => " +cF.getN());
+        Muve bSp2 = new Muve(1);
+        bSp2.getN();
+        System.out.println("Стартовый шпиль => " +bSp2.getN());
 
-//         // Muve.MuveS(an,bS,cF);
+        Muve cSp3 = new Muve(2);
+        cSp3.getN();
+        System.out.println("Финишный шпиль => " +cSp3.getN());
+
+        Muve(aN,aSp1, bSp2,cSp3);
         
-//     }
-// }
+    }
+
     
-//     // static double  MuveS(Muve an, Muve bS ,Muve cF) {
-//     //     if(an.getN()==1){
-//     //         return MuveS(an, bS, cF);
-//     //     } 
-//         // else {
-//         //     return TempOritMo(6, 3, 2);
+    static double  MuveS(Muve an, Muve bS ,Muve cF) {
+        if(an.getN()==1){
+            return MuveS(an, bS, cF);
+        } 
+        else {
+            return TempOritMo(6, 3, 2);
             
-//         // }
+        }
 
         
     
-//     // static double TempOritMo(int Temp1, int bS, int cF){
-//     //     int tmp = 0;
-//     //     tmp = (Temp1 - bS- cF);
-//     //     return tmp;
-//     // }
-//         // if (n == 1){
-//             // System.out.printf("%d ", n , Start1 ,  Finish1);
-//         // } 
-//         // else Muve(n-1, Start, Finish1); {
-//             // System.out.printf("%d " + "%d " + "%d ", n,  Start1, Finish1);
-//         //     int Temp1 =  6 - Start1 - Finish1;
-//         //     Muve(n-1, Temp1, Finish1);
-//         // }
-         
-    
+    static double TempOritMo(int Temp1, int bS, int cF){
+        int tmp = 0;
+        tmp = (Temp1 - bS- cF);
+        return tmp;
+        }
+        if (n == 1){
+            System.out.printf("%d ", n , Start1 ,  Finish1);
+        } 
+        else Muve(n-1, Start, Finish1); {
+            System.out.printf("%d " + "%d " + "%d ", n,  Start1, Finish1);
+            int Temp1 =  6 - Start1 - Finish1;
+            Muve(n-1, Temp1, Finish1);
+        }
+    }        
+}
 
     
 
